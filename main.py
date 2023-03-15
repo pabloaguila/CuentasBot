b87186b0e6f3a299ba757afeada632ade7e63ee6
Pablo <pabloaguila97@gmail.com>
2023-03-15 12:03:29 -0300

    Change add_expenditure function to a command.
    I changed the handler of the add_expenditure function from a
    MessageHandler to a CommandHandler. Now we get a list with every
    word that's in the message and it's easier to find the data of
    a expenditure.

--- main.py
+++ main.py
@@ -18,36 +18,52 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Soy un bot que te permite llevar registro de tus gastos.")
+    welcome_text="I help you keep track of your expenses."
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_text)
 
 
 async def add_expenditure(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """This function receives the update from the handler (which should've 
+    filtered for text messages with a hashtag entity) and gets the 
+    information about the expenditure. Finally it adds that information
+    to a csv file (WIP: use a database).
     """
-    Descripción de la función
-    """
-    texto_mensaje = update.message.text
-    match_precio = re.search(r"(\$\d+)", texto_mensaje)#busca donde aparece el precio
-    precio = match_precio.group()[1:]
-    precio = int(precio)
-    descripcion = texto_mensaje[match_precio.span()[1]+1:]#dos lugares después del precio está la descripción
-    fecha = update.message.date
+    expenditure_data = context.args
+    price = float("NaN")
+    category = ""
+    description = ""
+    for data in expenditure_data:
+        if data.startswith("$"):
+            price = int(data[1:])
+        elif data.startswith("#"):
+            category = data[1:]
+        else:
+            description += " "+data
+    description = description.strip()        
+    #match_price = re.search(r"(\$\d+)", text)#find the price
+    #price = match_price.group()[1:]
+    #price = int(price)
     
-    #diría de sacar esto y buscar en el texto del mensaje. Lo que sobra del hashtag y el precio es la descripcion 
-    entidad_hashtag = update.message.parse_entities() #busca el hashtag que es un tipo especial en telegram
-    tipo = list(entidad_hashtag.items())[0][1][1:] 
+    #description = text[match_price.end():]+text[:match_price.start()]#remove price from string
+    #match_category = re.search(r"(\#\w+)", description) #find the category
+    #category = match_category.group()[1:]
+
+    #description = description[match_category.end():]+description[:match_category.start()] #remove category from description
+    #description = description.strip()
+
+    date = update.message.date#find date
     
     #I want to implement this part with a database and not a .csv file
     df = pd.read_csv("gastos.csv")#find the expenditures file and add the new expenditure
-    df.loc[len(df.index)] = [precio, tipo, descripcion, fecha]
+    df.loc[len(df.index)] = [price, category, description, date]
     df.to_csv("gastos.csv", index=False)
     
-
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
-    expenditure_handler = MessageHandler(filters.TEXT & (~filters.COMMAND) & filters.Entity("hashtag"), add_expenditure)
+    expenditure_handler = CommandHandler("expenditure", add_expenditure)
 
     application.add_handler(start_handler)
     application.add_handler(expenditure_handler)
