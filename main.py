cb2ac5b014733d3e337536dfd9a883f52093ea70
Pablo <pabloaguila97@gmail.com>
2023-06-01 20:02:59 -0300

    Modifiqué main.py para que guarde los datos según la zona horaria de Argentina

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from telegram import Update
 import mysql.connector as msc
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
-import SQL_utils #I wrote this module with the functions that can interact with a SQL database
+# I wrote this module with the functions that can interact with a SQL database
+import SQL_utils
 import re
 import pandas as pd
 """
 This is a bot that helps you track your expenditures.
-It's a personal project but I hope it will eventually be hosted on a server and available to everybody. 
+It's a personal project but I hope it will eventually be hosted on a server and available to everybody.
 """
 load_dotenv()
-SQL_utils.SQL_USER=os.environ.get("SQL_USER")
-SQL_utils.SQL_PASSWORD=os.environ.get("SQL_PASSWORD")
-SQL_utils.SQL_DATABASE=os.environ.get("SQL_DATABASE")
-
+SQL_utils.SQL_USER = os.environ.get("SQL_USER")
+SQL_utils.SQL_PASSWORD = os.environ.get("SQL_PASSWORD")
+SQL_utils.SQL_DATABASE = os.environ.get("SQL_DATABASE")
+timedelta_utc_hours = timedelta(hours=-3)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -23,17 +25,21 @@ logging.basicConfig(
 )
 
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    welcome_text="I help you keep track of your expenses."
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_text)
+    welcome_text = "I help you keep track of your expenses."
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text=welcome_text)
+
 
 class AmountTypeError(TypeError):
     pass
 
-def add_expenditure(update: Update, context: ContextTypes.DEFAULT_TYPE, trans_type):
-    """This function receives the update from the handler as a command and gets the 
-    information about the expenditure. Finally it adds that information
+
+def add_expenditure(update: Update,
+                    context: ContextTypes.DEFAULT_TYPE,
+                    trans_type):
+    """This function receives the update from the handler as a command and gets
+    the information about the expenditure. Finally it adds that information
     to a SQL database.
     """
     expenditure_data = context.args
@@ -45,7 +51,7 @@ def add_expenditure(update: Update, context: ContextTypes.DEFAULT_TYPE, trans_ty
         if data.startswith("$"):
             try:
                 amount = int(data[1:])
-            except:
+            except Exception:
                 raise AmountTypeError
         elif data.startswith("#"):
             category = data[1:]
@@ -53,28 +59,30 @@ def add_expenditure(update: Update, context: ContextTypes.DEFAULT_TYPE, trans_ty
             account = data[1:]
         else:
             description += " "+data
-    description = description.strip()          
-    date = update.message.date#find date
+    description = description.strip()
+    date = update.message.date  # find date
+    date = date+timedelta_utc_hours  # change dateime according to timezone
     SQL_utils.add_transaction(amount, category, description, date, account, trans_type)
 
+
 async def create_account(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """This function receives the update from the handler as a command and gets the 
+    """This function receives the update from the handler as a command and gets the
     information of a new account. If everything is ok it adds a new account to a SQL database.
     """
     account_data = context.args
     balance = 0
-    
+
     try:
         name = str(account_data[0])
     except IndexError:
-        await context.bot.send_message(chat_id=update.effective_chat.id, 
+        await context.bot.send_message(chat_id=update.effective_chat.id,
                                        text="You have to write the account name next to the /create_account command.")
         return
     except Exception as err:
         print(err)
         return
-    
-    try:#try to add the values to a database
+
+    try:  # try to add the values to a database
         SQL_utils.add_account(name, balance)
     except SQL_utils.DuplicateAccountNameError:
         await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -87,7 +95,7 @@ async def create_account(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def add_debit(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """This function receives the update from the handler as a command and gets the 
+    """This function receives the update from the handler as a command and gets the
     information about the debit. Finally it adds that information
     to a SQL database.
     """
@@ -104,11 +112,11 @@ async def add_debit(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Debit registered successfully.")
 
 async def add_credit(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """This function receives the update from the handler as a command and gets the 
+    """This function receives the update from the handler as a command and gets the
     information about the credit. Finally it adds that information
     to a SQL database.
     """
-    try:#try to add the values to a database
+    try:# try to add the values to a database
         add_expenditure(update, context, trans_type="Credit")
     except SQL_utils.AccountNameError:
         await context.bot.send_message(chat_id=update.effective_chat.id,
@@ -128,10 +136,10 @@ if __name__ == '__main__':
     debit_handler = CommandHandler("debit", add_debit)
     credit_handler = CommandHandler("credit", add_credit)
     create_account_handler = CommandHandler("create_account", create_account)
-    
+
     application.add_handler(start_handler)
     application.add_handler(debit_handler)
     application.add_handler(credit_handler)
     application.add_handler(create_account_handler)
-    
+
     application.run_polling()
